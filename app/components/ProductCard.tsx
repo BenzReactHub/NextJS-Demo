e2dd2d463b16c1c1825e6ed3501f8747e927c9a2
lenrich <lenrich1403@gmail.com>
2024-01-10 11:28:31 +0800

    18.5- DaisyUI

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import AddToCart from "./AddToCart";
-// styles 是Javascript Objects
-import styles from './ProductCard.module.css'
 
 const ProductCard = () => {
   return (
-    <div className={styles.card}>
+    <div>
       <AddToCart />
     </div>
   );
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from "react";
 
 interface User {
   id: number;
-  name: string
+  name: string;
+  email: string;
 }
 
 // 注意，這邊的渲染是發生在server上面
 const UsersPage = async () => {
   // 經常跟新的data，我們就不要做緩存
-  const res = await fetch('https://jsonplaceholder.typicode.com/users', { cache: 'no-store' });
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    cache: "no-store",
+  });
   // 每十秒從Next.js後端緩存提取data到前端，這樣可以大幅降低對API的調用
   // 但是cache只能用在fetch，axiox沒有這個功能
   // const res = await fetch('https://jsonplaceholder.typicode.com/users', {next: { revalidate: 10 }});
@@ -17,12 +20,24 @@ const UsersPage = async () => {
   return (
     <>
       <h1>Users</h1>
-      <p>{new Date().toLocaleTimeString()}</p>
-      <ul>
-        {users.map(user=> <div key={user.id}>{user.name}</div>)}
-      </ul>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
-  )
-}
+  );
+};
 
-export default UsersPage
+export default UsersPage;
