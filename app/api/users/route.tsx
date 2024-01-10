46ee6e00256c8be3df88de2caedde138550d77ed
lenrich <lenrich1403@gmail.com>
2024-01-10 15:45:28 +0800

    33.4- Creating an Object

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -13,3 +13,14 @@ export function GET(request: NextRequest) {
     { id: 2, name: "lisa" },
   ]);
 }
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  // Validate
+  // if invalid, return 400
+  // else, return
+
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+}
