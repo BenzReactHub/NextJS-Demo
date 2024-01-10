7cc753a0822210dac8d527af7ef21bb9f1a34de1
lenrich <lenrich1403@gmail.com>
2024-01-10 20:10:56 +0800

    46.9- Updating Data

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -14,12 +14,19 @@ export async function GET(request: NextRequest) {
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
-  // Validate
-  // if invalid, return 400
-  // else, return
-
   const validation = schema.safeParse(body)
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
-  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+
+  const user = await prisma.user.findUnique({
+    where: {email: body.email}
+  })
+  if(user) return NextResponse.json({error: 'User already exists'}, {status: 400})
+  const newUser = await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+    }
+  })
+  return NextResponse.json(newUser, { status: 201 });
 }
