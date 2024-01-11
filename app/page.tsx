6ab5b6856777cb318cd68bc521caffd7b66c2c3c
lenrich <lenrich1403@gmail.com>
2024-01-11 16:38:12 +0800

    77.6- Lazy Loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,54 @@
+"use client";
 // import Link from 'next/link'
 // import ProductCard from './components/ProductCard'
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 import Image from "next/image";
 import { Metadata } from "next";
+// import dynamic from "next/dynamic";
+// const HeavyComponent = dynamic(() => import("./components/HeavyComponent"), {
+//   // 如果加載失敗的話，我們就禁止render
+//   ssr: false,
+//   loading: () => <p>Loading...</p>,
+// });
+
+
+// import _ from 'lodash';
+
+
+import { useState } from "react";
 // import man from "@/public/images/man.png";
 
-export default async function Home() {
-  const session = await getServerSession(authOptions);
+export default function Home() {
+  // const session = await getServerSession(authOptions);
+  // const [isVisible, setIsVisible] = useState(false);
   return (
     <main className="relative h-screen">
       <h1 className="font-poppins">Hello World</h1>
+      {/* <button
+        onClick={() => {
+          setIsVisible(true);
+        }}
+      >
+        Show
+      </button> */}
+
+      {/* lazy loading  */}
+      <button
+        onClick={async() => {
+          const _ = (await import('lodash')).default;
+          const users = [
+            {name: 'c'},
+            {name: 'b'},
+            {name: 'a'},
+          ];
+          const sortedArray = _.orderBy(users, ['name']);
+          console.log(sortedArray)
+        }}
+      >
+        Show
+      </button>
+      {/* {isVisible && <HeavyComponent />} */}
       {/* <h1>Hello World { session && <span>{session.user!.name}</span> }</h1>
 
       <Link href="/users">Users</Link>
@@ -18,7 +56,7 @@ export default async function Home() {
 
       {/* 圖片優化 */}
       {/* <Image src={man} alt='Man'/> */}
-      <Image
+      {/* <Image
         src="https://bit.ly/react-cover"
         alt="Man"
         // width={300}
@@ -36,7 +74,7 @@ export default async function Home() {
 
         // cancel lazy loading
         priority
-      />
+      /> */}
     </main>
   );
 }
@@ -46,10 +84,10 @@ export default async function Home() {
 //   // 這邊可以單頁dynamic生成metadata
 // }
 
-export async function generateMetadata(): Promise<Metadata> {
-  const product = await fetch('');
-  return {
-    title: 'product.title',
-    description: 'product.description'
-  }
-}
+// export async function generateMetadata(): Promise<Metadata> {
+//   const product = await fetch('');
+//   return {
+//     title: 'product.title',
+//     description: 'product.description'
+//   }
+// }
