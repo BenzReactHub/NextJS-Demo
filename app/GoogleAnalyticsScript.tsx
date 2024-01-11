01294067376db8f7019e58bf498c1eca54a43ab9
lenrich <lenrich1403@gmail.com>
2024-01-11 15:59:33 +0800

    74.3- Using Third-party Scripts(Google tag)

---
+++ app/GoogleAnalyticsScript.tsx
@@ -0,0 +1,24 @@
+import Script from "next/script";
+import React from "react";
+
+const GoogleAnalyticsScript = () => {
+  return (
+    <>
+      {/* Google tag (gtag.js) */}
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-P8ER86JVW2"
+      />
+      {/* beforeInteractive的意思是下面的代碼在Next.js進入點注入，也稱為hydration */}
+      <Script id="googleTag" strategy="beforeInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-P8ER86JVW2');`}
+      </Script>
+    </>
+  );
+};
+
+export default GoogleAnalyticsScript;
