86ed40901f6bc20373c90b3b42b46aff367ea14e
lenrich <lenrich1403@gmail.com>
2024-01-11 11:10:15 +0800

    57.3- Configuring Google Provider

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -8,6 +8,7 @@ const NavBar = () => {
         <Link href="/users">Users</Link>
         <Link href="/products">Products</Link>
         <Link href="/admin">Admin</Link>
+        <Link href="/api/auth/signin">Login</Link>
     </div>
   )
 }
