4126feaea895ed90dce65b281332b84343481f93
lenrich <lenrich1403@gmail.com>
2024-01-11 12:13:19 +0800

    63.9- Database Adapters

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,23 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import { Adapter } from "next-auth/adapters";
 
-export const authOptions = {
+const prisma = new PrismaClient();
+
+const handler =  NextAuth({
+  adapter: PrismaAdapter(prisma) as Adapter,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-};
-
-const handler = NextAuth(authOptions);
+  session: {
+    strategy: "jwt",
+  },
+});
 
 export { handler as GET, handler as POST };
+
